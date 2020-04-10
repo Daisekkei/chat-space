fd80095bd7c3180b2e50b0c65e67917c62c5f1f3
Daisekkei <58997423+Daisekkei@users.noreply.github.com>
2020-04-10 19:32:37 +0900

    自動更新機能実装
    What
    リロードせずに自動でブラウザーが更新される機能を実施
    Why
    チャット機能として、その他のユーザーの投稿を自動で更新する機能が必要であるため

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,43 +1,43 @@
 $(function(){ 
 
   function buildHTML(message){
-   if ( message.image ) {
-     var html =
-      `<div class="message">
-         <div class="upper-message">
-           <div class="upper-message__user-name">
-             ${message.user_name}
-           </div>
-           <div class="upper-message__date">
-             ${message.created_at}
-           </div>
+    if ( message.image ) {
+    var html =
+    `<div class="message" data-message-id=${message.id}>
+       <div class="upper-message">
+         <div class="upper-message__user-name">
+           ${message.user_name}
          </div>
-         <div class="lower-message">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
+         <div class="upper-message__date">
+           ${message.created_at}
          </div>
-         <img src=${message.image} >
-       </div>`
+       </div>
+       <div class="lower-message">
+         <p class="lower-message__content">
+           ${message.content}
+         </p>
+       </div>
+       <img src=${message.image} >
+     </div>`
      return html;
    } else {
-     var html =
-      `<div class="message">
-         <div class="upper-message">
-           <div class="upper-message__user-name">
-             ${message.user_name}
-           </div>
-           <div class="upper-message__date">
-             ${message.created_at}
-           </div>
+    var html =
+    `<div class="message" data-message-id=${message.id}>
+       <div class="upper-message">
+         <div class="upper-message__user-name">
+           ${message.user_name}
          </div>
-         <div class="lower-message">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
+         <div class="upper-message__date">
+           ${message.created_at}
          </div>
-       </div>`
-     return html;
+       </div>
+       <div class="lower-message">
+         <p class="lower-message__content">
+           ${message.content}
+         </p>
+       </div>
+     </div>`
+   return html;
    };
 }
 
@@ -67,5 +67,36 @@ $('#new_message').on('submit', function(e){
   $('.message-form__send-btn').prop('disabled', false);
   })
 })
-})
+
+var reloadMessages = function() {
+  //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+  var last_message_id = $('.message:last').data("message-id");
+  $.ajax({
+    //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+    url: "api/messages",
+    //ルーティングで設定した通りhttpメソッドをgetに指定
+    type: 'get',
+    dataType: 'json',
+    //dataオプションでリクエストに値を含める
+    data: {id: last_message_id}
+  })
+  .done(function(messages) {
+    //追加するHTMLの入れ物を作る
+    if (messages.length !== 0) {
+      var insertHTML = '';
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      //メッセージが入ったHTMLに、入れ物ごと追加
+      $('.message-list').append(insertHTML);
+      $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight});
+    }
+  })
+  .fail(function() {
+    alert('error');
+  });
+};
+ setInterval(reloadMessages, 7000);
+});
 
